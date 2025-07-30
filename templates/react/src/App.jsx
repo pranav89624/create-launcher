9564d4a12291a9ecc67f376382797a863a6d7bdd
Pranav Verma <pranav12340987@gmail.com>
2025-07-30 14:27:47 +0530

    Add simple react template

---
+++ templates/react/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CreateLauncher from "./assets/create-launcher.png";
+
+const App = () => {
+  return (
+    <div>
+      <img src={CreateLauncher} alt="Create Launcher" />
+
+      <h1>Welcome Developer !!!</h1>
+      <p>This is a simple React application.</p>
+      <p>Feel free to modify the code and explore!</p>
+      <p>Happy coding!</p>
+
+      <p className='footer'>Project created using <i>Create Launcher</i></p>
+    </div>
+  )
+}
+
+export default App
