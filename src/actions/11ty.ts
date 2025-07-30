57b36f66166309b7c6268e563db0317072bfd6a6
Pranav Verma <pranav12340987@gmail.com>
2025-07-30 15:17:29 +0530

    Add 11ty starter template

--- src/actions/11ty.ts
+++ src/actions/11ty.ts
@@ -1,3 +1,5 @@
-export async function create11tyApp(projectName: string) {
+import { copyTemplate } from "../utils/templateHelper.js";
 
+export async function create11tyApp(projectName: string) {
+    await copyTemplate("11ty", projectName);
 }
