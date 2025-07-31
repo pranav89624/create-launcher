71e84cb6d8c6bff104c930d32eaa155b39c2dd6f
Pranav Verma <pranav12340987@gmail.com>
2025-07-31 19:07:30 +0530

    Add React + TypeScript + Tailwind template

---
+++ templates/react-ts-tailwind/README.md
@@ -0,0 +1,33 @@
+# 🚀 React + TypeScript + Tailwind Starter (Vite)
+
+This is a modern React starter template bootstrapped using [`create-launcher`](https://github.com/pranav89624/create-launcher), a zero config CLI to scaffold your next project in seconds.
+
+---
+
+## 📦 Tech Stack
+
+- ⚛️ Framework: React 19
+- ⚡ Bundler: Vite
+- 🧠 Language: TypeScript
+- 💅 Styling: Tailwind CSS
+
+---
+
+## 📁 Project Structure
+
+```
+src/
+├── assets/          # Static assets (images, icons, etc.)
+├── App.tsx          # Main application component
+├── main.tsx         # Entry point for Vite
+├── index.css        # Global styles
+└── vite-env.d.ts    # Vite environment variables (This should not be modified)
+```
+
+## 🤖 Powered by create-launcher
+
+> Skip boilerplate. Skip tutorials. Just launch.
+
+```bash
+npx create-launcher
+```
---
+++ templates/react-ts-tailwind/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import CreateLauncher from "./assets/create-launcher.png";
+
+const App = () => {
+  return (
+    <div>
+      <img
+        className="h-[40vh] w-auto object-contain relative left-1/2 transform -translate-x-1/2"
+        src={CreateLauncher}
+        alt="Create Launcher"
+      />
+
+      <h1 className="text-[3rem] mb-[1rem]">Welcome Developer !!!</h1>
+      <p className="text-[1rem] text-justify">
+        This is a simple React + TypeScript + Tailwind application.
+      </p>
+      <p className="text-[1rem] text-justify">
+        Feel free to modify the code and explore!
+      </p>
+      <p className="text-[1rem] text-justify">Happy coding!</p>
+
+      <p className="text-[1rem] text-white absolute bottom-5 right-5">
+        Project created using <i>Create Launcher</i>
+      </p>
+    </div>
+  );
+};
+
+export default App;
