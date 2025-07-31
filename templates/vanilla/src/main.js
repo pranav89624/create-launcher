2b02843518802d233ac77be59cc561a174f71874
Pranav Verma <pranav12340987@gmail.com>
2025-07-31 15:22:47 +0530

    Update vanilla template

--- templates/vanilla/src/main.js
+++ templates/vanilla/src/main.js
@@ -6,7 +6,7 @@ document.querySelector('#app').innerHTML = `
     <img src="${createLauncher}" alt="Logo" class="logo" />
 
     <h1>Welcome Developer !!!</h1>
-    <p>This is a simple 11ty application.</p>
+    <p>This is a simple Vanilla JavaScript (Vite) application.</p>
     <p>Feel free to modify the code and explore!</p>
     <p>Happy coding!</p>
 
