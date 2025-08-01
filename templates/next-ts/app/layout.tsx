d502b7ad80a476d77e2119d3b7dbcdf689ede8f0
Pranav Verma <pranav12340987@gmail.com>
2025-08-01 19:11:10 +0530

    Add Next + Typescript

---
+++ templates/next-ts/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Next.js + TypeScript Starter",
+  description: "A simple Next.js + TypeScript application by Create Launcher",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="./icon.png" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
